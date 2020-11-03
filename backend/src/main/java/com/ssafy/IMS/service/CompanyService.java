461be9de4805fe36264bdbc51e2d0878d352ef67
배재원 <pageone0820@naver.com>
2020-11-03 13:41:41 +0900

    수정

--- backend/src/main/java/com/ssafy/IMS/service/CompanyService.java
+++ backend/src/main/java/com/ssafy/IMS/service/CompanyService.java
@@ -4,12 +4,16 @@ import com.ssafy.IMS.model.Company;
 import com.ssafy.IMS.payload.ApiResponse;
 import com.ssafy.IMS.payload.CompanyRequest;
 
+import java.util.List;
+
 public interface CompanyService {
     Company getCompany(String name);
 
     Company addCompany(CompanyRequest companyRequest);
 
     Company updateCompany(CompanyRequest companyRequest);
+
+    List<Company> getCompanyByRank(String rank1, String rank2);
 
     ApiResponse deleteCompany(int id);
 }
--- backend/src/main/java/com/ssafy/IMS/service/impl/CompanyServiceImpl.java
+++ backend/src/main/java/com/ssafy/IMS/service/impl/CompanyServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -61,6 +62,13 @@ public class CompanyServiceImpl implements CompanyService {
 
         return companyRepository.findByName(companyRequest.getName())
                 .orElseThrow(() -> new ResourceNotFoundException("Company", "name", companyRequest.getName()));
+    }
+
+    @Override
+    public List<Company> getCompanyByRank(String rank1, String rank2) {
+        List<Company> companies = companyRepository.findByRank1AndRank2(rank1, rank2)
+                .orElseThrow(() -> new ResourceNotFoundException("Company", "name", "not found"));
+        return companies;
     }
 
     @Override

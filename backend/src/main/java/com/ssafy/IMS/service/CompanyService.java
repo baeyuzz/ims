89c081e5e2fc92c110c3f1769501cc8851ad7afe
배재원 <pageone0820@naver.com>
2020-11-03 13:04:32 +0900

    회사정보 crud 제작

--- backend/src/main/java/com/ssafy/IMS/service/CompanyService.java
+++ backend/src/main/java/com/ssafy/IMS/service/CompanyService.java
@@ -1,7 +1,15 @@
 package com.ssafy.IMS.service;
 
 import com.ssafy.IMS.model.Company;
+import com.ssafy.IMS.payload.ApiResponse;
+import com.ssafy.IMS.payload.CompanyRequest;
 
 public interface CompanyService {
     Company getCompany(String name);
+
+    Company addCompany(CompanyRequest companyRequest);
+
+    Company updateCompany(CompanyRequest companyRequest);
+
+    ApiResponse deleteCompany(int id);
 }

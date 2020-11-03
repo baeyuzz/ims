15fa80ec8a4bc9b6ceae4aeacf506135b3a9f690
배재원 <pageone0820@naver.com>
2020-11-03 09:29:39 +0900

    essay crud created

--- backend/src/main/java/com/ssafy/IMS/repository/EssayRepository.java
+++ backend/src/main/java/com/ssafy/IMS/repository/EssayRepository.java
@@ -5,6 +5,13 @@ import com.ssafy.IMS.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotBlank;
+import java.util.Optional;
+
 @Repository
 public interface EssayRepository extends JpaRepository<Essay, Integer> {
+    Optional<Essay> findByUser(@NotBlank User user);
+    Optional<Essay> findByContent(@NotBlank String content);
+
 }
+

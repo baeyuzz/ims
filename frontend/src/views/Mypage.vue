96d2c1976c3933cc6c02487030d5c6bce3f3463c
배유진 <seow0124@naver.com>
2020-11-07 01:01:31 +0900

    20
    Former-commit-id: 0816b144f447715afb045d9ac90f52d53b0a6479

--- frontend/src/views/Mypage.vue
+++ frontend/src/views/Mypage.vue
@@ -1,48 +1,66 @@
 <template>
   <v-app>
-    <v-main>
-      <div class="cal">
-        <h1>일정 관리</h1>
-        <Calendar />
-        <v-divider style="margin-bottom:50px; margin-top:50px"></v-divider>
-        <h1>자기소개서 관리</h1>
-        <v-list>
-          <v-row v-for="(essay, index) in essays" :key="essay.id">
-            <v-col
-              style="text-align:left; cursor:pointer"
-              cols="11"
-              @click="gotoEssay(index)"
-            >
-              {{ essay.content.substring(0, 50) }}...
-            </v-col>
-            <v-col cols="1">
-              <v-icon @click="deleteEssay(index)">mdi-delete</v-icon>
-            </v-col>
-          </v-row>
-        </v-list>
-        <v-divider style="margin-bottom:20px; margin-top:50px"></v-divider>
-        <v-btn
-          color="error"
-          class="mr-4"
-          @click="signout"
-          dark
-          style="float:right; margin-bottom:20px"
-        >
-          회원탈퇴
-        </v-btn>
-        <v-btn
-          color="primary"
-          class="mr-4"
-          @click="dialog = true"
-          dark
-          style="float:right"
-        >
-          회원정보수정
-        </v-btn>
-        <v-dialog v-model="dialog" max-width="500">
-          <Signup @close="close"></Signup>
-        </v-dialog>
-      </div>
+    <v-main style="margin: 0; padding: 0">
+      <template>
+        <v-card>
+          <v-toolbar flat color="primary" dark>
+            <v-toolbar-title>My Page</v-toolbar-title>
+          </v-toolbar>
+          <v-tabs>
+            <v-tab>
+              <v-icon left> mdi-text </v-icon>
+            </v-tab>
+            <v-tab>
+              <v-icon left> mdi-calendar </v-icon>
+            </v-tab>
+            <v-tab>
+              <v-icon left> mdi-account </v-icon>
+            </v-tab>
+
+            <v-tab-item>
+              <br />
+              <h2 style="width: 90%; margin: auto">나의 자기소개서 관리</h2>
+              <br />
+              <v-list style="width: 90%; margin: auto; padding: auto">
+                <v-row v-for="(essay, index) in essays" :key="essay.id">
+                  <v-col
+                    style="text-align: left; cursor: pointer"
+                    cols="11"
+                    @click="gotoEssay(index)"
+                  >
+                    📃 {{ essay.content.substring(0, 50) }}...
+                  </v-col>
+                  <v-col cols="1">
+                    <v-icon @click="deleteEssay(index)">mdi-delete</v-icon>
+                  </v-col>
+                </v-row>
+              </v-list>
+            </v-tab-item>
+            <v-tab-item>
+              <br />
+              <h2 style="width: 90%; margin: auto">나의 일정 관리</h2>
+              <br />
+              <calendar style="width: 90%; margin: auto" />
+            </v-tab-item>
+            <v-tab-item>
+              <br />
+              <h2 style="width: 90%; margin: auto">회원정보 수정</h2>
+              <br />
+
+              <Signup />
+              <v-btn
+                color="error"
+                class="ma-10"
+                @click="signout"
+                dark
+                style="float: right"
+              >
+                회원탈퇴
+              </v-btn>
+            </v-tab-item>
+          </v-tabs>
+        </v-card>
+      </template>
     </v-main>
   </v-app>
 </template>
@@ -57,26 +75,26 @@ export default {
   data() {
     return {
       essays: [],
-      dialog: false
+      dialog: false,
     };
   },
   components: {
     Calendar: () => import("@/components/Calendar"),
-    Signup
+    Signup,
   },
   mounted() {
     const scope = this;
     getEssayByUser(
       this.$store.state.id,
-      function(response) {
+      function (response) {
         scope.essays = response.data;
       },
-      function(error) {
-        console.log(error);
+      function (error) {
+        // console.log(error);
       }
     );
   },
-  methods: {
+ methods: {
     gotoEssay(index) {
       let list = [];
       list.push(this.essays[index].result1);
@@ -90,9 +108,11 @@ export default {
 
       this.$store.commit("setResult", list);
 
-      console.log(list);
+      // console.log(list);
 
-      this.$router.push("/ai-result");
+      this.$store.commit("setContent",this.essays[index].content)
+
+      this.$router.push("/myEssay");
     },
     deleteEssay(index) {
       var scope = this;
@@ -138,6 +158,7 @@ export default {
   }
 };
 </script>
+
 <style scoped>
 .cal {
   margin: auto;

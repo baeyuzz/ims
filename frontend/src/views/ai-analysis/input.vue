af7eef6deb28dd524609c3622a174b6e0ee9feb5
배유진 <seow0124@naver.com>
2020-11-07 01:04:15 +0900

    FE - media query 적용
    Former-commit-id: 35f5322007ac711bc4d13241a7aeb3e027305812

--- frontend/src/views/ai-analysis/input.vue
+++ frontend/src/views/ai-analysis/input.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="input">
     <img src="@/assets/ai-intro.png" width="100%" />
+    <v-toolbar class="toolbar" color="primary" dark>
+      <v-toolbar-title>AI-Analysis</v-toolbar-title>
+    </v-toolbar>
     <div class="text">
       <ai-input />
     </div>
@@ -20,15 +23,40 @@ export default {
   width: 65%;
   margin: auto;
 }
-
 .intro {
   background-color: #ebf0ff;
   color: #1c2341;
   padding: 70px;
   text-align: center;
 }
-
 #h2 {
   font-size: 20px;
+}
+.toolbar {
+  display: none;
+}
+@media (max-width: 950px) {
+  .essay {
+    width: 90%;
+    margin: auto;
+    padding: 3% 0 5% 0;
+  }
+  img {
+    display: none;
+  }
+  .toolbar {
+    display: block;
+  }
+  .text {
+    width: 95%;
+  }
+}
+@media (max-width: 450px) {
+  img {
+    display: none;
+  }
+  .toolbar {
+    display: block;
+  }
 }
 </style>
